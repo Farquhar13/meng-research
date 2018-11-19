bf406c90cf37108594be934f14f650040b836eaa
Collin Farquhar <33133635+Farquhar13@users.noreply.github.com>
2018-11-19 15:31:54 -0500

    refactoring

--- src/first_pytorch.py
+++ src/first_pytorch.py
@@ -6,6 +6,8 @@ Author: Collin Farquhar
 Description: First implementation of a pytorch NN
 '''
 
+# import modules
+import argparse # for OptionParser
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -15,26 +17,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+class OptionParser():
+    def __init__(self):
+        """User based option parser"""
+        self.parser = argparse.ArgumentParser(prog='PROG')
+        self.parser.add_argument("--fin", action="store",
+            dest="fin", default="", help="Input file")
+        self.parser.add_argument("--fout", action="store",
+            dest="fout", default="", help="Output file")
+        self.parser.add_argument("--verbose", action="store_true",
+            dest="verbose", default=False, help="verbose output")
 
+        # for input data as argument
+        self.parser.add_argument("--data", action="store", dest="data",
+                default=False, help="import local data set" )
 
-a = 1
-# load CIFAR10 image dataset and transfors to range [-1. 1] 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 # implement NN class
 class Net(nn.Module):
@@ -48,6 +45,7 @@ class Net(nn.Module):
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
+        """forward propogate using relu activtion"""
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = x.view(-1, 16 * 5 * 5)
@@ -56,49 +54,84 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
-net = Net()
+def load_CIFAR10():
+    """load CIFAR10 image dataset and transfors to range [-1. 1]"""
+    transform = transforms.Compose([transforms.ToTensor(), 
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-# specify loss function and pass params to opimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
 
-# Learn!
-for epoch in range(2):  # loop over the dataset multiple times
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
 
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs
-        inputs, labels = data
+    classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+    return trainloader, testloader, classes
 
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+def custom_data():
+   """handels data passed as argument and returns train and test data""" 
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
 
-print('Finished Training')
+def main():
 
-# compute 0/1 loss
-correct = 0
-total = 0
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+    # load data, default or given
+    optmgr  = OptionParser()
+    args = optmgr.parser.parse_args()
+    if args.data:
+        train_data, test_data, classes = custom_data()
+    else:
+        train_data, test_data, classes = load_CIFAR10()
+    
+    net = Net()
+    # specify loss function and pass params to opimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-    100 * correct / total))
+    # Learn!
+    for epoch in range(2):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(train_data, 0):
+            # get the inputs
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print("done training")        
+    
+    # compute 0/1 loss
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_data:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+
+if __name__ == '__main__':
+    main()
+
 
